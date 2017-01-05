f42d41ee297c93dbbd84dcf4b53079899b90aa04
skylerl <skylerl@indellient.com>
2017-01-05 13:35:41 -0500

    implement the email interface
    Signed-off-by: skylerl <skylerl@indellient.com>

---
+++ src/main/java/me/skylerlayne/mail/impl/SESMailDeliverer.java
@@ -0,0 +1,59 @@
+package me.skylerlayne.mail.impl;
+
+import me.skylerlayne.mail.EmailDeliverer;
+
+/**
+ * AWS SES Implementation.
+ * 
+ * @author skylerl
+ * @version 0.1.0
+ *
+ */
+public class SESMailDeliverer implements EmailDeliverer {
+
+	public void setFrom(String from) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setTo(String to) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setSubject(String subject) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setBody(String body) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void send() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public EmailDeliverer addFrom(String from) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public EmailDeliverer addTo(String to) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public EmailDeliverer addSubject(String subject) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public EmailDeliverer addBody(String body) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
